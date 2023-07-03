09503061d1aba9ba6daedf8c7c6b1bdf85058f92
Chris Rudnicky <crudnicky@absci.com>
2023-07-02 23:13:29 -0700

    perf: :racehorse: add custom logger method

---
+++ app/lib/utils/log.py
@@ -0,0 +1,12 @@
+def logger(text: str) -> None:
+    print("==========")
+    print("==========")
+    print("==========")
+    print(" ")
+    print(" ")
+    print(text)
+    print(" ")
+    print(" ")
+    print("==========")
+    print("==========")
+    print("==========")
