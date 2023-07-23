919b4f96f8dee955a6e7a148ee78845e99f86df0
Chris Rudnicky <crudnicky@absci.com>
2023-07-23 10:01:44 -0700

    refactor: move bill routes to endpoint dir

--- app/api/endpoints/bills.py
+++ app/api/endpoints/bills.py
@@ -1,14 +1,33 @@
 from fastapi import APIRouter, Depends
-from typing import List, Any
-from app.models.all import Bill
-from app.api import deps
-from sqlalchemy.orm import Session
-from app import crud
+from typing import List
+from app.models.all import Bill, BillCreate
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlmodel import select
+from app.db.db import get_session, init_db
 
 router = APIRouter(prefix="/bills")
 
 
-@router.get("/", operation_id="get_bills")  # response_model=List[Bill]
-async def get_bills(db: Session = Depends(deps.get_db)) -> Any:
-    bills = await crud.bill.get_multiple(db)
+@router.get("/", operation_id="get_bills", response_model=List[Bill])
+async def get_bills(session: AsyncSession = Depends(get_session)):
+    result = await session.execute(select(Bill))
+    bills = result.scalars().all()
     return bills
+
+
+@router.post("/", operation_id="add_bill", response_model=Bill)
+async def add_bill(bill: BillCreate, session: AsyncSession = Depends(get_session)):
+    new_bill = Bill(
+        name=bill.name,
+        amount=bill.amount,
+        due_date=bill.due_date,
+        frequency=bill.frequency,
+        recurring=bill.recurring,
+        category=bill.category,
+        status=bill.status,
+        notes=bill.notes,
+    )
+    session.add(new_bill)
+    await session.commit()
+    await session.refresh(new_bill)
+    return new_bill
--- app/main.py
+++ app/main.py
@@ -5,31 +5,12 @@ from app.db.juno_tables import JunoTables
 from dotenv import load_dotenv
 from uuid import UUID
 
-# from app.api.v1_routes import api_router
-
-from sqlmodel.ext.asyncio.session import AsyncSession
-from app.models.all import Bill, BillCreate
-from sqlmodel import select
-from app.db.db import get_session, init_db
+from app.api.v1_routes import api_router
 
 load_dotenv()
 
 
-# @asynccontextmanager
-# async def lifespan(app: FastAPI):
-#     """async function ensures async connections use the same event loop as the app"""
-#     # * execute before startup
-#     services = {}
-#     services["juno_tables"] = JunoTables()
-#     # await services["juno_tables"].drop_tables()
-#     await services["juno_tables"].create_tables()
-#     yield
-#     # * clean up and release resources on shutdown
-#     await services["juno_tables"].close()
-
-
-# app = FastAPI(title="Juno API", lifespan=lifespan)
-app = FastAPI()
+app = FastAPI(title="Juno API")
 
 
 app.add_middleware(
@@ -40,29 +21,4 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# app.include_router(api_router)
-
-
-@app.get("/bills")
-async def get_bills(session: AsyncSession = Depends(get_session)):
-    result = await session.execute(select(Bill))
-    bills = result.scalars().all()
-    return [Bill(name=bill.name, id=bill.id) for bill in bills]
-
-
-@app.post("/bills")
-async def add_bill(bill: BillCreate, session: AsyncSession = Depends(get_session)):
-    new_bill = Bill(
-        name=bill.name,
-        amount=bill.amount,
-        due_date=bill.due_date,
-        frequency=bill.frequency,
-        recurring=bill.recurring,
-        category=bill.category,
-        status=bill.status,
-        notes=bill.notes,
-    )
-    session.add(new_bill)
-    await session.commit()
-    await session.refresh(new_bill)
-    return new_bill
+app.include_router(api_router)
